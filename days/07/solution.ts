a0bed5c01fbc547625bad8e0627be3aeddf610bd
Josh Dysiewicz <joshuadysiewicz@googlemail.com>
2024-12-07 09:38:22 +0000

    almost there, just need to turn the combinatino checker into a generator so JS can garbage collect it correctly

--- days/07/solution.ts
+++ days/07/solution.ts
@@ -1,5 +1,7 @@
 import { getLines } from "../../utils/getLines";
 import { solutionPrinter } from "../../utils/solutionPrinter";
+import * as permutation from "array-permutation";
+
 type Inputs = number[];
 
 interface ResultMap {
@@ -7,33 +9,76 @@ interface ResultMap {
 }
 
 const part1 = () => {
-  const lines = getLines("./days/07/test-input.txt");
+  const lines = getLines("./days/07/input.txt");
 
-  const solvableResults: number[] = [];
   const resultMap = createResultInputMap(lines);
 
+  let sum = 0;
   for (let [k, v] of Object.entries(resultMap)) {
     const target = parseInt(k);
     for (let input of v) {
       const isSolveable = determineIfSolvable(target, input);
       if (isSolveable) {
-        solvableResults.push(target);
+        sum += target;
       }
     }
   }
-
-  const sum = solvableResults.reduce((acc, curr) => acc + curr, 0);
 
   return sum;
 };
 
 const determineIfSolvable = (target: number, inputs: Inputs): boolean => {
-  // Always -1 as always 1 less operator than number of inputs
-  const operatorCombinations = getAllOperatorCombinations(inputs.length - 1);
+  // Short circuit
+  const sum = inputs.reduce((acc, curr) => acc + curr, 0);
+  if (sum > target) {
+    return false;
+  }
+  const product = inputs.reduce((acc, curr) => acc * curr, 1);
+  if (product < target) {
+    return false;
+  }
 
-  // Try each combinatino of operators on the inputs left-to-right evaluation
-  // if any match then return true
+  // Always -1 as always 1 less operator than number of inputs
+  console.log(`CHECKING ${target} ${inputs}`);
+  const operatorCombinations = getAllOperatorCombinations(inputs.length - 1);
+  let checked = 0;
+
+  for (let i = 0; i < operatorCombinations.length; i++) {
+    console.log(`Checked ${checked} of ${operatorCombinations.length}`);
+    console.log(target, inputs, operatorCombinations[i]);
+    if (target === getResultOfCombination(inputs, operatorCombinations[i])) {
+      return true;
+    }
+    checked++;
+  }
+
   return false;
+};
+
+// [1,2,3]
+// [+,*]
+
+// i=0; 1, +
+// i=1; 2, *
+// i=2;3, out of bounds
+
+const getResultOfCombination = (
+  inputs: Inputs,
+  combination: Operator[]
+): number => {
+  let res = 0;
+  for (let i = 0; i < inputs.length; i++) {
+    if (i === 0) {
+      res = inputs[0];
+    }
+    if (combination[i - 1] === "+") {
+      res += inputs[i];
+    } else if (combination[i - 1] === "*") {
+      res *= inputs[i];
+    }
+  }
+
+  return res;
 };
 
 const getAllOperatorCombinations = (size: number): Operator[][] => {
@@ -50,10 +95,24 @@ const getAllOperatorCombinations = (size: number): Operator[][] => {
     baseArrays.push(arr);
   }
 
-  // Get all permuations of the base arrays
-  // then push each unique permutation to the results
+  console.log(baseArrays);
 
-  return results;
+  for (let i = 0; i < baseArrays.length; i++) {
+    const perms = combinateWithoutReplacement(baseArrays[i]);
+    // const iter = permutation.permutation(baseArrays[i]);
+    console.log(perms);
+    for (let res of perms) {
+      results.push(res);
+    }
+  }
+
+  // dedupe via string conversion of array
+  const dedupe = Array.from(
+    new Set(results.map((r) => JSON.stringify(r))),
+    (a) => JSON.parse(a)
+  );
+
+  return dedupe;
 };
 
 type Operator = "+" | "*";
@@ -61,6 +120,9 @@ type Operator = "+" | "*";
 const createResultInputMap = (lines: string[]): ResultMap => {
   const res: ResultMap = {};
   for (let line of lines) {
+    if (line.length === 0) {
+      continue;
+    }
     const splt = line.split(":");
     const result = splt[0];
     const inputs = splt[1]
@@ -83,3 +145,35 @@ const part2 = () => {
 
 solutionPrinter(7, 1, part1());
 solutionPrinter(7, 2, part2());
+
+export function* combinateWithoutReplacement<T>(candidates: T[]) {
+  const indices = candidates.map((_, i) => i);
+
+  yield indices.map((i) => candidates[i]);
+
+  const c = indices.map(() => 0);
+  let i = 1;
+  while (i < indices.length) {
+    if (c[i] < i) {
+      if (i % 2 === 0) {
+        swap(indices, 0, i);
+      } else {
+        swap(indices, c[i], i);
+      }
+
+      yield indices.map((i) => candidates[i]);
+
+      c[i] += 1;
+      i = 1;
+    } else {
+      c[i] = 0;
+      i += 1;
+    }
+  }
+}
+
+function swap<T>(list: T[], a: number, b: number) {
+  const t = list[a];
+  list[a] = list[b];
+  list[b] = t;
+}
